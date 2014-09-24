7860b2ec369dd2cedcaba294957cd3e173d8efc2
Matt Watson <Matt.Watson@orasi.com>
2014-09-24 16:03:07 -0400

    more adfs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :wsfed,
     :issuer_name           => "http://adfs.orasi.com",
-    :issuer                => "https://adfs.orasi.com",
+    :issuer                => "https://adfs.orasi.com/federationmetadata/2007-06/federationmetadata.xml",
     :realm                 => "http://codechallenge.orasi.com",
     :reply                 => "http://codechallenge.orasi.com/auth/wsfed/callback",
     :id_claim              => "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name",
