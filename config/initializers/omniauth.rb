2d5aa76cd14cbe010cea22895da3316f4ae646a9
Matt Watson <Matt.Watson@orasi.com>
2014-09-24 16:34:46 -0400

    more

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     :issuer                => "https://adfs.orasi.com/adfs/ls/",
     :realm                 => "http://codechallenge.orasi.com",
     :reply                 => "http://codechallenge.orasi.com/login",
-    :id_claim              => "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name",
     :idp_cert_fingerprint  => "FC96D2983…"
 
 end
