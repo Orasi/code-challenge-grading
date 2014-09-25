55737b97cafb2152a37061086ea8947098eb0c29
Matt Watson <Matt.Watson@orasi.com>
2014-09-25 16:20:14 -0400

    more

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -14,7 +14,7 @@ end
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :saml,
     :assertion_consumer_service_url     => "https://codechallenge.orasi.com/auth/saml/callback",
-    :issuer                             => "codechallenge",
+    :issuer                             => "https://codechallenge.orasi.com",
     :idp_sso_target_url                 => "https://adfs.orasi.com/adfs/ls/",
     :idp_cert_fingerprint               => "DF:36:3E:72:B1:36:D8:8E:32:55:41:B8:92:39:A9:03:7C:08:8F:88"
 
